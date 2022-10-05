d4ac849103b330e08c82cf8b40c38a6d8a821df1
Oryzapp Blanche <clsuspecialpurposerice@gmail.com>
2022-10-06 06:44:52 +0800

    Topbar Layout

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,9 +1,17 @@
 export default function Topbar() {
-    return (
-        <div className=" h-10  my-2 -ml-4  mr-2 pl-10 flex opacity-90 rounded-xl sm:bg-white">
-            <div className=" sm:hidden">hamburger</div>
-            <div className=""> h1 </div>
-            <div>Profile with Photo</div>
-        </div>
-    )
+  return (
+    <div className=" h-10  my-2 -ml-4  mr-2 pl-10 pr-2 flex items-center space opacity-90 rounded-xl sm:bg-white">
+      {/* <div className="">hamburger</div>
+      <div className="">Logo,Oryzapp</div>
+      <div className="flex gap-2 justify-center">
+        
+      </div> */}
+      <div className="sm:hidden bg-blue-600 h-6 w-full">hamburger</div>
+      <div className="bg-blue-400 h-6 w-full">Oryzapp</div>
+      <div className="bg-blue-200 h-6 w-full flex gap-2 justify-end">
+        <p>Kumusta, Juaan</p>
+        <div className="h-6 w-6 rounded-full bg-black"></div>
+      </div>
+    </div>
+  );
 }
--- src/pages/RiceList.js
+++ src/pages/RiceList.js
@@ -1,56 +1,73 @@
-import { collection, collectionGroup, onSnapshot } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import db from '../firebase-config';
+import { collection, collectionGroup, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import db from "../firebase-config";
 export default function RiceList() {
+  const [riceList, setRiceList] = useState([]);
 
-    const [riceList, setRiceList] = useState([]);
+  // console.log(riceList);
 
-    // console.log(riceList);
+  useEffect(() => {
+    const riceCollectionRef = collectionGroup(db, "Raw_Rice_Data");
+    onSnapshot(riceCollectionRef, (snapshot) => {
+      setRiceList(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
-    useEffect(() => {
-        const riceCollectionRef = collectionGroup(db, 'Raw_Rice_Data');
-        onSnapshot(riceCollectionRef, (snapshot) => {
-            setRiceList(snapshot.docs.map(doc => doc.data()));
-        }
-        )
-    }, []);
-
-    return (
-        <>
-            {/* Header */}
-            <header className="h-5 bg-blue-600 font-bold">Rice List</header>
-            {/* Options */}
-            <div className="flex h-10 bg-blue-600 ">
-                <div>Search</div>
-                <div>Filter</div>
-            </div>
-            {/* Main Section */}
-            <section className="rounded-lg bg-blue-600">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Accession</th>
-                            <th>Season</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {
-                            riceList.map(rice => (
-                                <tr>
-                                    <td>{rice.id}</td>
-                                    <td>{rice.season}</td>
-                                    <td>{rice.year}</td>
-                                </tr>
-
-                            ))
-                        }
-
-
-                    </tbody>
-                </table>
-            </section>
-        </>
-    )
+  return (
+    <>
+      {/* Header */}
+      <header className="h-5 bg-blue-600 font-bold">Rice List</header>
+      {/* Options */}
+      <div className="flex h-10 bg-blue-600 ">
+        <div className="relative">
+          <div className="absolute right-0 rounded-full bg-red-300 w-6 h-6">
+            +
+          </div>
+          <input
+            className=" pl-2 rounded-full"
+            type="text"
+            placeholder="Find a Rice"
+          />
+        </div>
+        <div className="relative">
+          Filter
+          <div className=" absolute w-28 h-auto rounded-sm p-2 z-50  bg-white">
+            <label className="block" htmlFor="">
+              <input type="checkbox" name="Season" id="Season" />
+              Season
+            </label>
+            <label className="block" htmlFor="">
+              <input type="checkbox" name="Season" id="Season" />
+              Year
+            </label>
+            <label className="block" htmlFor="">
+              <input type="checkbox" name="Season" id="Season" />
+              Variety
+            </label>
+          </div>
+        </div>
+      </div>
+      {/* Main Section */}
+      <section className="rounded-lg bg-blue-600">
+        <table>
+          <thead>
+            <tr>
+              <th>Accession</th>
+              <th>Season</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            {riceList.map((rice) => (
+              <tr>
+                <td>{rice.id}</td>
+                <td>{rice.season}</td>
+                <td>{rice.year}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
 }
