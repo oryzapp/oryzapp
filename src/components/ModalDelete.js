2fa42680f205a6c190a6250fea5eb95e3ebc27f0
iKulot <96379253+iKulot@users.noreply.github.com>
2022-11-29 11:05:18 +0800

    batch delete done

--- src/components/ModalDelete.js
+++ src/components/ModalDelete.js
@@ -1,4 +1,4 @@
-import { writeBatch, collection, deleteDoc, doc, onSnapshot, query, where } from 'firebase/firestore';
+import { writeBatch, collection, deleteDoc, doc, onSnapshot, query, where, collectionGroup } from 'firebase/firestore';
 import React from 'react'
 import ReactDom from 'react-dom'
 import closeIcon from "../assets/close.svg"
@@ -11,44 +11,170 @@ import { useState } from 'react';
 const ModalDelete = ({ open, closeModal, modalId, delId }) => {
 
     const [dryList, setDryList] = useState([])
+    const [wetList, setWetList] = useState([])
+    const [vsDryList, setVsDryList] = useState([])
+    const [vsWetList, setVsWetList] = useState([])
+    const [rsDryList, setRsDryList] = useState([])
+    const [rsWetList, setRsWetList] = useState([])
+    const [gcDryList, setGcDryList] = useState([])
+    const [gcWetList, setGcWetList] = useState([])
+    const [ycDryList, setYcDryList] = useState([])
+    const [ycWetList, setYcWetList] = useState([])
 
     useEffect(() => {
 
-        const collectionRef = collection(db, "SPR/Rice_Accessions/Rice_List/Dry_Season/Raw_Rice_List/");
 
-        const dryList = query(collectionRef, where("accessionId", "==", modalId))
-        const unsub = onSnapshot(dryList, (snapshot) => {
+
+        const dryList = query(collection(db, "SPR/Rice_Accessions/Rice_List/Dry_Season/Raw_Rice_List/"), where("accessionId", "==", modalId))
+        onSnapshot(dryList, (snapshot) => {
             setDryList(
                 snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
             );
         });
+
+        const wetList = query(collection(db, "SPR/Rice_Accessions/Rice_List/Wet_Season/Raw_Rice_List/"), where("accessionId", "==", modalId))
+        onSnapshot(wetList, (snapshot) => {
+            setWetList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+
+        // Vegetative Stage
+        const vsDryList = query(collection(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Vegetative_Stage/VS_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(vsDryList, (snapshot) => {
+            setVsDryList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        const vsWetList = query(collection(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Vegetative_Stage/VS_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(vsWetList, (snapshot) => {
+            setVsWetList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        // Reproductive Stage
+        const rsDryList = query(collection(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Reproductive_Stage/RS_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(rsDryList, (snapshot) => {
+            setRsDryList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        const rsWetList = query(collection(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Reproductive_Stage/RS_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(rsWetList, (snapshot) => {
+            setRsWetList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+
+        // Grain Characteristics
+        const gcDryList = query(collection(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Grain_Characteristics/GC_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(gcDryList, (snapshot) => {
+            setGcDryList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        const gcWetList = query(collection(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Grain_Characteristics/GC_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(gcWetList, (snapshot) => {
+            setGcWetList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        // Yield Components
+        const ycDryList = query(collection(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Yield_Components/YC_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(ycDryList, (snapshot) => {
+            setYcDryList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+        const ycWetList = query(collection(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Yield_Components/YC_Raw_Rice_Data"), where("accessionId", "==", modalId))
+        onSnapshot(ycWetList, (snapshot) => {
+            setYcWetList(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            );
+        });
+
+
+
     }, [modalId])
 
 
-    console.log('I am dryList' + dryList);
-    dryList.map((item) => {
-        console.log(item.id);
+    // console.log('I am dryList' + dryList);
+    // dryList.map((item) => {
+    //     console.log(item.id);
+
+    // })
+    // console.log('I am wetList' + wetList);
+    wetList.map((item) => {
+        // console.log(item.id);
+
+    })
+    // console.log('I am VS DRY LIST' + vsDryList);
+    vsDryList.map((item) => {
+        // console.log(`vs` + item.id);
+
+    })
+    console.log('I am gc DRY LIST' + vsDryList);
+    gcDryList.forEach((item) => {
+        console.log(`gc` + item.id);
 
     })
 
     const deleteRiceAccession = async (accessionId, id) => {
         try {
             console.log('Iam' + accessionId);
+            const batch = writeBatch(db)
+
+
             const docRef = doc(db, "SPR/Rice_Accessions/Accession_IDs", id);
-
-            const dryBatch = writeBatch(db)
-
 
 
             dryList.forEach((rice) => {
-                const dryDocRef = doc(db, "SPR/Rice_Accessions/Rice_List/Dry_Season/Raw_Rice_List", rice.id)
-                dryBatch.delete(dryDocRef)
+                batch.delete(doc(db, "SPR/Rice_Accessions/Rice_List/Dry_Season/Raw_Rice_List", rice.id))
+            })
+            wetList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Accessions/Rice_List/Wet_Season/Raw_Rice_List", rice.id))
             })
 
-            await dryBatch.commit()
-            await deleteDoc(docRef);
-            return alert('Accession Deleted')
+            // Vegetative Stage
+            vsWetList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Vegetative_Stage/VS_Raw_Rice_Data", rice.id))
+                console.log('vsWetDelete:' + rice.id);
+            })
+            vsDryList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Vegetative_Stage/VS_Raw_Rice_Data", rice.id))
+                console.log('vsDryDelete:' + rice.id);
 
+            })
+            // Reproductive Stage
+            rsWetList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Reproductive_Stage/RS_Raw_Rice_Data", rice.id))
+                console.log('rsWetDelete:' + rice.id);
+
+            })
+            rsDryList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Reproductive_Stage/RS_Raw_Rice_Data", rice.id))
+                console.log('rsDryDelete:' + rice.id);
+
+            })
+            // Grain Characteristics
+            gcWetList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Grain_Characteristic/GC_Raw_Rice_Data", rice.id))
+            })
+            gcDryList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Grain_Characteristics/GC_Raw_Rice_Data", rice.id))
+            })
+            // Yield Components
+            ycWetList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Wet_Season/Stages/Yield_Components/YC_Raw_Rice_Data", rice.id))
+            })
+            ycDryList.forEach((rice) => {
+                batch.delete(doc(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Yield_Components/YC_Raw_Rice_Data", rice.id))
+            })
+
+            await batch.commit()
+            await deleteDoc(docRef);
+
+            return alert('Accession Delted')
 
         } catch (error) {
             console.log(error);
@@ -88,3 +214,18 @@ const ModalDelete = ({ open, closeModal, modalId, delId }) => {
 }
 
 export default ModalDelete
+
+// vsDryList.forEach((rice) => {
+//     console.log('vsDryDelete:' + rice.id);
+
+//     const docRef = doc(db, "SPR/Rice_Seasons/Seasons/Dry_Season/Stages/Vegetative_Stage/VS_Raw_Rice_Data", rice.id)
+
+//     deleteDoc(docRef).then(() => {
+//         console.log("Entire Document has been deleted successfully.")
+//     })
+//         .catch(error => {
+//             console.log(error);
+//         })
+//     return alert('Accession Deleted')
+
+// })
